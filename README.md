16b6695cea3e373e96e53f5f92244b2b66679191
Bennyc0 <108821644+Bennyc0@users.noreply.github.com>
2023-04-28 17:57:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# code-next-plain-js-counter-dgzyzz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/code-next-plain-js-counter-ewgjrj)
---
+++ counter.js
@@ -0,0 +1,6 @@
+// TODO: Add counter code here.
+const countSpan = document.getElementById('count');
+
+const increment_btn = document.querySelector('#increment')
+const decrement_btn = document.querySelector('#decrement')
+
